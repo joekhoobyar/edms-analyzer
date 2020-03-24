f7f2b3931e121062932b172f0829aa173bf0ecef
Joe Khoobyar <fourheads@gmail.com>
2020-03-23 21:42:28 -0400

    improved logging

--- lib/edms/analyzer.rb
+++ lib/edms/analyzer.rb
@@ -5,6 +5,13 @@ require 'edms'
 module EDMS
   # Parent namespace for the analyzer microservice.
   module Analyzer
+
+    # Namespace holding rack env constants.
+    module RackEnv
+      ASYNC_LOGGER       = 'async.logger'
+      ASYNC_LOGGER_TOPIC = 'async.logger.topic'
+    end
+
     autoload :Web, 'edms/analyzer/web'
   end
 end

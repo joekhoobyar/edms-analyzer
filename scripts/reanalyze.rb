5d8a83615c597b263d1d6570bf942ed43799077c
Joe Khoobyar <fourheads@gmail.com>
2022-04-14 23:01:01 -0400

    script no longer needs to retrieve text

--- scripts/reanalyze.rb
+++ scripts/reanalyze.rb
@@ -17,23 +17,11 @@ Async do
 
     ARGV.each do |id|
       puts "retrieving document ##{id}"
-
       mayan_doc = client.document(id)
       body = {
         id: mayan_doc.value[:id],
         type: mayan_doc.value[:document_type][:id],
       }
-
-      body[:text] = mayan_doc.first_file.content
-      body[:text] = mayan_doc.latest_version.ocr_content if (body[:text]&.strip&.length || 0) == 0
-
-      body[:text] = body[:text].
-        scrub.
-        force_encoding('UTF-8').
-        gsub(/[^[:ascii:]]/i, ' ').
-        gsub(/[^[:alnum:][:punct:][:space:]]/i, ' ').
-        gsub(/[[:space:]]/i, ' ').
-        strip
 
       puts "reanalyzing document ##{id}"
       url = ENV.fetch('ANALYZER_URL', 'http://localhost:9292/analyses/documents')

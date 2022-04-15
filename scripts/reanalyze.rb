891db12dab6587facb197671beabb2f62c84b821
Joe Khoobyar <fourheads@gmail.com>
2022-04-14 22:40:03 -0400

    add text scrubbing

--- scripts/reanalyze.rb
+++ scripts/reanalyze.rb
@@ -11,9 +11,9 @@ require 'edms/analyzer'
 
 Async do
 	internet = Async::HTTP::Internet.new
-	headers = [%w(Content-type application/json)]
+	headers = [['content-type', 'application/json; charset=UTF-8']]
 
-  EDMS::MayanDecorator.new.send(:with_client) do |client|
+  EDMS::MayanDecorator.new.with_client do |client|
 
     ARGV.each do |id|
       puts "retrieving document ##{id}"
@@ -24,8 +24,16 @@ Async do
         type: mayan_doc.value[:document_type][:id],
       }
 
-      body[:text] = mayan_doc.first_file.content&.strip
-      body[:text] = mayan_doc.latest_version.ocr_content if (body[:text]&.length || 0) == 0
+      body[:text] = mayan_doc.first_file.content
+      body[:text] = mayan_doc.latest_version.ocr_content if (body[:text]&.strip&.length || 0) == 0
+
+      body[:text] = body[:text].
+        scrub.
+        force_encoding('UTF-8').
+        gsub(/[^[:ascii:]]/i, ' ').
+        gsub(/[^[:alnum:][:punct:][:space:]]/i, ' ').
+        gsub(/[[:space:]]/i, ' ').
+        strip
 
       puts "reanalyzing document ##{id}"
       url = ENV.fetch('ANALYZER_URL', 'http://localhost:9292/analyses/documents')
